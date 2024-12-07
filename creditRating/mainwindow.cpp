daa84f4b183d6312cad8e773bf3d1459f80938ca
mininim <minsoplee2015@gmail.com>
2024-12-07 16:25:49 +0900

    mainwindow 유저데이터, 신용평가데이터 모델 추가

--- creditRating/mainwindow.cpp
+++ creditRating/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include <QVBoxLayout>
-#include "DataProcessor.h"
+#include <vector>
+#include <string>
+
 // 각자 헤더 추가
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
@@ -10,54 +12,63 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     QWidget *centralWidget = new QWidget(this);
     this->setCentralWidget(centralWidget);
 
-    customerTable = new QTableWidget(2, 2, this);
+    // 데이터 초기화
+    std::vector<std::pair<QString, QString>> customerData = {
+        {"1", "왕쌍치"},
+        {"2", "유리리"},
+        {"3", "왕우"},
+    };
+
+    std::vector<std::tuple<QString, QString, QString>> companyData = {
+        {"A", "신용정보 조회서", "공과금"},
+        {"B", "신용정보 조회서", "통신요금"},
+        {"B", "신용정보 조회서", "통신요금, 이것저것"}
+    };
+
+    // 고객 테이블
+    customerTable = new QTableWidget(static_cast<int>(customerData.size()), 2, this);
     customerTable->setHorizontalHeaderLabels({"ID", "이름"});
-    customerTable->setItem(0, 0, new QTableWidgetItem("asw222"));
-    customerTable->setItem(0, 1, new QTableWidgetItem("왕쌍치"));
-    customerTable->setItem(1, 0, new QTableWidgetItem("12asdc"));
-    customerTable->setItem(1, 1, new QTableWidgetItem("유리리"));
+    for (size_t i = 0; i < customerData.size(); ++i) {
+        customerTable->setItem(static_cast<int>(i), 0, new QTableWidgetItem(customerData[i].first));
+        customerTable->setItem(static_cast<int>(i), 1, new QTableWidgetItem(customerData[i].second));
+    }
 
-    customerTable->setSelectionBehavior(QAbstractItemView::SelectRows);    // 设置整行选择模式
-    customerTable->setSelectionMode(QAbstractItemView::SingleSelection);    // 限制只能选中一行
-
+    customerTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+    customerTable->setSelectionMode(QAbstractItemView::SingleSelection);
     customerTable->setStyleSheet(
         "QTableWidget::item:selected {"
-        "   background-color: #f7dc6f;" // 设置背景颜色为黄色
-        "   color: black;"             // 设置字体颜色为黑色
+        "   background-color: #f7dc6f;"
+        "   color: black;"
         "}"
-        );
-    customerTable->setFocusPolicy(Qt::NoFocus);  // 禁用表格的焦点策略（移除黑线框）
+    );
+    customerTable->setFocusPolicy(Qt::NoFocus);
 
-
-
-    companyTable = new QTableWidget(2, 3, this);
+    // 신용평가회사 테이블
+    companyTable = new QTableWidget(static_cast<int>(companyData.size()), 3, this);
     companyTable->setHorizontalHeaderLabels({"평가사 이름", "금융 데이터", "비금융 데이터"});
-    companyTable->setItem(0, 0, new QTableWidgetItem("A"));
-    companyTable->setItem(0, 1, new QTableWidgetItem("신용정보 조회서"));
-    companyTable->setItem(0, 2, new QTableWidgetItem("공과금"));
-    companyTable->setItem(1, 0, new QTableWidgetItem("B"));
-    companyTable->setItem(1, 1, new QTableWidgetItem("신용정보 조회서"));
-    companyTable->setItem(1, 2, new QTableWidgetItem("통신요금"));
+    for (size_t i = 0; i < companyData.size(); ++i) {
+        companyTable->setItem(static_cast<int>(i), 0, new QTableWidgetItem(std::get<0>(companyData[i])));
+        companyTable->setItem(static_cast<int>(i), 1, new QTableWidgetItem(std::get<1>(companyData[i])));
+        companyTable->setItem(static_cast<int>(i), 2, new QTableWidgetItem(std::get<2>(companyData[i])));
+    }
 
-    companyTable->setSelectionBehavior(QAbstractItemView::SelectRows);    // 设置整行选择模式
-    companyTable->setSelectionMode(QAbstractItemView::SingleSelection);    // 限制只能选中一行
-
+    companyTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+    companyTable->setSelectionMode(QAbstractItemView::SingleSelection);
     companyTable->setStyleSheet(
         "QTableWidget::item:selected {"
-        "   background-color: #f7dc6f;" // 设置背景颜色为黄色
-        "   color: black;"             // 设置字体颜色为黑色
+        "   background-color: #f7dc6f;"
+        "   color: black;"
         "}"
         "QTableWidget::item {"
-        "    outline: none;" // 移除黑线框
+        "    outline: none;"
         "}"
-        );
-    companyTable->setFocusPolicy(Qt::NoFocus);  // 禁用表格的焦点策略
+    );
+    companyTable->setFocusPolicy(Qt::NoFocus);
 
-
+    // 버튼 및 레이블 설정
     generateKeyButton = new QPushButton("키 생성하기", this);
-    generateKeyButton -> setFixedSize(120,35);
+    generateKeyButton->setFixedSize(120, 35);
     generateKeyButton->setStyleSheet("font-size: 15px; font-weight: bold;");
-
 
     selectedInfoLabel = new QLabel("<span style='font-size: 14px;'>"
                                    "선택된 고객: 없음"
@@ -65,9 +76,10 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
                                    "신용평가 회사: 없음", this);
     scoreLabel = new QLabel("<span style='font-size: 16px;'>점수: --", this);
     checkScoreButton = new QPushButton("신용평가 점수 확인하기", this);
-    checkScoreButton -> setFixedSize(160,35);
-    generateKeyButton->setStyleSheet("font-size: 15px; font-weight: bold;");
+    checkScoreButton->setFixedSize(160, 35);
+    checkScoreButton->setStyleSheet("font-size: 15px; font-weight: bold;");
 
+    // 레이아웃 구성
     QVBoxLayout *leftLayout = new QVBoxLayout;
     leftLayout->addWidget(new QLabel("<span style='font-size: 14.5px; color: #1f618d;'>고객 목록", this));
     leftLayout->addWidget(customerTable);
@@ -85,9 +97,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     infoLayout->addWidget(checkScoreButton);
 
     QHBoxLayout *buttonLayout = new QHBoxLayout;
-    buttonLayout->addStretch(); // 左侧弹性空白
+    buttonLayout->addStretch();
     buttonLayout->addWidget(scoreLabel);
-    buttonLayout->addStretch(); // 右侧弹性空白
+    buttonLayout->addStretch();
 
     QVBoxLayout *mainLayout = new QVBoxLayout;
     mainLayout->addWidget(generateKeyButton);
@@ -110,11 +122,6 @@ void MainWindow::checkScore() {
     int customerRow = customerTable->currentRow();
     int companyRow = companyTable->currentRow();
 
-    // 각자 연산 결과 가져오고 
-    // int yuchan = 0;
-    // int minsop = 0;
-
-
     if (customerRow == -1 || companyRow == -1) {
         selectedInfoLabel->setText("<span style='font-size: 14px;'>고객과 신용평가 회사를 선택해주세요");
         scoreLabel->setText("<span style='font-size: 14px;'>점수: --");
@@ -127,9 +134,9 @@ void MainWindow::checkScore() {
     selectedInfoLabel->setText(
         QString("<span style='font-size: 14px;'>"
                 "선택된 고객: <span style='color: red;'>%1</span>"
-                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"  //空格
-                "신용평가 회사: <span style='color: red;'>%2</span> ").arg(customerId, companyName));
-
+                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                "신용평가 회사: <span style='color: red;'>%2</span>").arg(customerId, companyName));
 
     scoreLabel->setText("<span style='font-size: 16px;'>점수: 100");
 }
+
